178e43253257708adbfd37ebceb0ec6b819d4623
Ilona <dobro.ilona@yandex.ru>
2023-07-24 17:23:21 +0300

    HomeWork TestNG

---
+++ src/test/java/CashbackHackServiceTest.java
@@ -0,0 +1,40 @@
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import ru.netology.service.CashbackHackService;
+
+public class CashbackHackServiceTest {
+
+    @Test
+    public void Test1() {
+        CashbackHackService service =  new CashbackHackService();
+        int amount = 900;
+
+        int actual = service.remain(amount);
+        int expected = 100;
+
+        Assert.assertEquals(actual, expected);
+    }
+
+    @Test
+    public void Test2() {
+        CashbackHackService service =  new CashbackHackService();
+        int amount = 3500;
+
+        int actual = service.remain(amount);
+        int expected = 500;
+
+        Assert.assertEquals(actual, expected);
+    }
+
+    @Test
+    public void Test3() {
+        CashbackHackService service =  new CashbackHackService();
+        int amount = 1000;
+
+        int actual = service.remain(amount);
+        int expected = 1000;
+
+        Assert.assertEquals(actual, expected);
+    }
+}
